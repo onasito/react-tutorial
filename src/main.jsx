cc8bde047b16d9ead1f41f803f62b6ee6eb0780e
Onasis A. <oarrechavala0@gmail.com>
2024-07-23 15:42:08 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('main')).render(
+  <React.StrictMode><div>Hello World</div></React.StrictMode>,
+)
