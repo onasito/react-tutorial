c13306c9feaaa8e77d16d761f48784b5a40fc3e2
Onasis A. <oarrechavala0@gmail.com>
2024-08-04 15:57:31 -0700

    Registering window events

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { LoginForm } from "./components/LoginForm";
 
 export default function App() {
+
+  window.addEventListener('resize', (e) => {
+    console.log(window.innerHeight, window.innerWidth);
+  });
+
   return (
     <div>
       <LoginForm />
