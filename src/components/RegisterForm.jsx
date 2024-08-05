dbd559e46457ad3c852ad3c183f562be3acfd37d
Onasis A. <oarrechavala0@gmail.com>
2024-08-05 16:09:47 -0700

    Learned how to create an object that holds all the fields and how to update the fields within this object using a spread operator

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,45 +1,57 @@
 import {useState} from 'react';
 
 export function RegisterForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [displayName, setDisplayName] = useState("");
 
+  const [formFields, setFormFields] = useState({
+    username: "",
+    pasword: "",
+    displayName: "",
+  })
+
+  console.log(formFields)
+   
   return (
     <form>
       <div>
         <label htmlFor="username">Username</label>
         <input 
           id="username" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)}
+          value={formFields.username} 
+          onChange={(e) => {
+            setFormFields((currentState)=> ({...currentState, username: e.target.value}))
+          }}
         />
       </div>
       <div>
         <label htmlFor="password">Password</label>
         <input 
           id="password" 
-          value={password} 
-          onChange={(e)=> setPassword(e.target.value)}
+          value={formFields.password} 
+          onChange={(e)=> {
+            setFormFields((currentState)=> ({...currentState, password: e.target.value}))
+          }}
         />
       </div>
       <div>
         <label htmlFor="displayName">Display Name</label>
         <input 
           id="displayName" 
-          value={displayName} 
-          onChange={(e)=> setDisplayName(e.target.value)}
+          value={formFields.displayName} 
+          onChange={(e)=> {
+            setFormFields((currentState)=> ({...currentState, displayName: e.target.value}))
+          }}
         />
       </div>
       <div>
-        <span>Username: {username}</span>
+        <span>Username: {formFields.username}</span>
       </div>
       <div>
-        <span>Password: {password}</span>
+        <span>Password: {formFields.pasword}</span>
       </div>
       <div>
-        <span>Display Name: {displayName}</span>
+        <span>Display Name: {formFields.displayName}</span>
       </div>
+      {/*<button disabled={isDisabled}>Sign Up</button>*/}
     </form>
   );
 }
