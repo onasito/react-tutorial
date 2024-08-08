d522ffa7149cd6976453cdba51df60e562608c66
Onasis A. <oarrechavala0@gmail.com>
2024-08-07 19:05:28 -0700

    Learned about state with arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { RegisterForm } from "./components/RegisterForm";
+import { useState } from "react";
+import { UserDetails } from "./components/UserDetails";
 
 export default function App() {
-
-  window.addEventListener('resize', (e) => {
-    console.log(window.innerHeight, window.innerWidth);
-  });
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "anson",
+      email: "anson@gmail.com"
+    },
+    {
+      id: 2,
+      username: "mike",
+      email: "mike@gmail.com"
+    },
+  ]);
 
   return (
-    <div>
-      <RegisterForm />
-    </div>
-  );
+  <div>
+    {users.map((user)=> <UserDetails key={user.id} user={user} setUsers={setUsers} />)}
+  </div>
+  )
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,17 +1,48 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-export function UserDetails({ user }) {
+export function UserDetails({ user, setUsers }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState(user.username);
+  const [email, setUserEmail] = useState(user.email);
+
+
   return (
     <div>
-      <b>ID: </b>
-      <span>{user.id}</span>
-      <br />
-      <b>Username: </b> 
-      <span>{user.username}</span>
-      <br />
-      <b>Email: </b>
-      <span>{user.email}</span>
-      <br />
+      <div>
+        <button 
+          onClick={() => {
+            setIsEditing((currentState) => !currentState);
+          }}
+        >
+          Edit
+        </button>
+        <button>Delete</button>
+        <button onClick={()=> {
+          
+        }}>Save</button>
+      </div>
+      <div>
+        <b>ID: </b>
+        <span>{user.id}</span>
+        <br />
+        <b>Username: </b> 
+        {isEditing ? <input name='username' id='username' value={username} onChange={(e) => {
+            setUsername(e.target.value)
+          }}  
+        />  : 
+          <span>{user.username}</span>
+        } 
+        <br />
+        <b>Email: </b>
+        {isEditing ? <input name='email' id='email' value={email} onChange={(e) => {
+            setUserEmail(e.target.value)
+          }} 
+        /> : 
+          <span>{user.email}</span>
+        }
+        <br />
+      </div>
     </div>
   );
 }
@@ -21,5 +52,7 @@ UserDetails.propTypes =  {
     id: PropTypes.number.isRequired,
     username: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
-  })
+  }).isRequired,
+
+  setUsers: PropTypes.func.isRequired,
 }
