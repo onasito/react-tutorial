bc043648dcb5c3275977b41e1e5ca81371b920ba
Onasis A. <oarrechavala0@gmail.com>
2024-08-11 16:09:45 -0700

    Learned how to add users to the user array

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { UserDetails } from "./components/UserDetails";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [counter, setCounter] = useState(3);
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -16,8 +20,36 @@ export default function App() {
   ]);
 
   return (
-  <div>
-    {users.map((user)=> <UserDetails key={user.id} user={user} setUsers={setUsers} />)}
-  </div>
-  )
+    <div>
+    <form 
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newUser = {
+            id: counter,
+            username,
+            email,
+          };
+          setCounter((currentCounter) => currentCounter + 1);
+          setUsers((currentUsersState) => [...currentUsersState, newUser]);
+        }}
+      >
+        <div>
+          <label htmlFor="username">Username</label>
+          <input name="username" id="username" value={username} onChange={(e) => {
+            setUsername(e.target.value);
+          }}/>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input name="email" id="email" value={email} onChange={(e) => {
+            setEmail(e.target.value);
+          }} />
+        </div>
+        <button>Add User</button>
+      </form>
+      <br />
+      {users.map((user)=> <UserDetails key={user.id} user={user} setUsers={setUsers} />)}
+    </div>
+  );
 }
